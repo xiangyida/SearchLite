fdf6b6a21322a1b652362aad2071265cd0dc6b5c
xiangyida <3331665905@qq.com>
2020-05-08 11:57:55 +0800

    用户反馈功能完成

--- pages/me/feedback/feedback.js
+++ pages/me/feedback/feedback.js
@@ -1,66 +1,32 @@
-// pages/feedback/feedback.js
+import Dialog from '@vant/weapp/dialog/dialog';
 Page({
-
-  /**
-   * 页面的初始数据
-   */
-  data: {
-
+  data:{
+    problem:'',
+    contact:'',
+    others:''
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  submit: function(){
+    let message = {};
+    message.problem = this.data.problem;
+    message.contact = this.data.contact;
+    message.others = this.data.others;
+    console.log(message)
+    wx.request({
+      url: 'http://127.0.0.1:8080/searchlite/user/feedback/',
+      method:'POST',
+      data: message,
+      success :res =>{
+        Dialog.alert({
+          message: '提交成功，感谢您的反馈'
+        }).then(() => {
+           wx.switchTab({
+             url: '../index/index',
+           })
+        });
+      }
+    })
   }
+
+
 })
